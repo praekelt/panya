3c0a2d927263dbfea076e398f6c2f2ba47738c84
Euan Jonker <euan@unomena.com>
2012-01-02 18:24:32 +0200

    Upgraded the django-cache-machine to ver.0.6.unomena.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     dependency_links = [
         'http://dist.plone.org/thirdparty/',
         'http://github.com/praekelt/django-photologue/tarball/2.6.praekelt#egg=django-photologue-2.6.praekelt',
-        'http://github.com/unomena/django-cache-machine/tarball/master#egg=django-cache-machine-0.4.1.unomena',
+        'http://github.com/unomena/django-cache-machine/tarball/0.6.unomena.1#egg=django-cache-machine-0.6.unomena.1',
     ],
     install_requires = [
         'PIL',
@@ -23,7 +23,7 @@ setup(
         'django-publisher',
         'django-secretballot',
         'python-memcached==1.48',
-        'django-cache-machine==0.4.1.unomena',
+        'django-cache-machine==0.6.unomena.1',
         'django-reversion==1.3.2',
     ],
     include_package_data=True,
