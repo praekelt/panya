7796dc18a1bebcd4d3fffab93eb8a79d89d7b798
Julian Mulder <git@nerdz.za.net>
2015-05-27 12:27:50 +0200

    bump django-reversion again

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         'django-secretballot',
         'python-memcached==1.48',
         'django-cache-machine==0.6.unomena.4',
-        'django-reversion==1.5',
+        'django-reversion==1.6',
     ],
     include_package_data=True,
     classifiers = [
